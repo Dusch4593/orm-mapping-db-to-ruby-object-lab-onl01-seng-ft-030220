0b10e8b8b17eaebe2ee8102aeed92119e74fa9d6
Brandon Dusch <brandondusch@gmail.com>
2020-04-04 13:04:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,16 @@ class Student
     new_student.grade = row[2]
     new_student
   end
+  
+  def self.all_students_in_grade_9 
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE students.grade = 9;
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      row 
+    end
+  end
 end
