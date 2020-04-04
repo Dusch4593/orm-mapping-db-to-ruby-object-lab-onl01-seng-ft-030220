e8b5ab426422de7a1000d287fe6fa9df97328a3d
Brandon Dusch <brandondusch@gmail.com>
2020-04-04 14:12:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,6 +117,16 @@ class Student
   def self.first_student_in_grade_10 
     self.first_X_students_in_grade_10(1)[0]
   end
+  
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE students.grade = ?;
+    SQL
+    
+    
+  end
 end
 
 
